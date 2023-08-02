269e49496d8be09386614f84cb8d8be4fdb35e79
HJJ <1916787042@qq.com>
2023-08-02 17:25:04 +0800

    fix: make output style universal

--- internal/tools/cypher_printer.go
+++ internal/tools/cypher_printer.go
@@ -11,7 +11,7 @@ func CypherPrinter(cyphers models.CredentialList, plaintext bool) {
 	table.SetHeader([]string{" 域名 ", " 用户名 ", " 密码 "})
 	table.SetHeaderColor(
 		tablewriter.Colors{tablewriter.FgWhiteColor, tablewriter.BgBlackColor},
-		tablewriter.Colors{tablewriter.FgHiBlackColor, tablewriter.BgHiRedColor},
+		tablewriter.Colors{tablewriter.FgBlackColor, tablewriter.BgRedColor},
 		tablewriter.Colors{tablewriter.FgBlackColor, tablewriter.BgGreenColor})
 	table.SetAlignment(tablewriter.ALIGN_LEFT)
 	for _, cipher := range cyphers {
