1e9c6ea305387db7c460b81a14671599dc6f7411
朱天利 <372736560@qq.com>
2024-01-30 16:54:06 +0800

    refactor: 调整密码明文显示交互

--- frontend/src/components/Cypher.vue
+++ frontend/src/components/Cypher.vue
@@ -21,6 +21,7 @@ const data = reactive({
   showPassword: ref(false),
   createOrEditDialogVisible: ref(false),
 });
+const pwdVisible = reactive({});
 
 const formRef = ref();
 const formRules = reactive({
@@ -49,8 +50,13 @@ const formRules = reactive({
 
 const form = reactive(defaultFormValue);
 
-const togglePasswordVisibility = () => {
-  data.showPassword = !data.showPassword;
+const togglePasswordVisibility = (domain) => {
+  pwdVisible[domain] = !pwdVisible[domain] ?? false;
+  if (pwdVisible[domain]) {
+    setTimeout(() => {
+      pwdVisible[domain] = false;
+    }, 1000 * 10);
+  }
 };
 
 const filterCyphers = computed(() =>
@@ -169,12 +175,6 @@ onMounted(() => {
               </el-select>
             </template>
           </el-input>
-          <el-button
-            class="visible-btn"
-            size="middle"
-            @click="togglePasswordVisibility"
-            >密明文切换</el-button
-          >
           <el-button type="primary" @click="handleAdd"> 添加 </el-button>
         </el-space>
       </div>
@@ -230,11 +230,43 @@ onMounted(() => {
                 show-password
               />
             </el-form-item>
-            <div v-else>
-              <span v-if="data.showPassword" disabled>{{
-                scope.row.password
+            <div class="el-custom-password-column" v-else>
+              <span class="ellipsis">{{
+                pwdVisible[scope.row.domain] ? scope.row.password : "******"
               }}</span>
-              <span v-else>******</span>
+              <span
+                class="el-custom-hover-icon"
+                @click="togglePasswordVisibility(scope.row.domain)"
+              >
+                <el-icon v-if="!pwdVisible[scope.row.domain]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1024 1024"
+                    data-v-ea893728=""
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M512 160c320 0 512 352 512 352S832 864 512 864 0 512 0 512s192-352 512-352m0 64c-225.28 0-384.128 208.064-436.8 288 52.608 79.872 211.456 288 436.8 288 225.28 0 384.128-208.064 436.8-288-52.608-79.872-211.456-288-436.8-288zm0 64a224 224 0 1 1 0 448 224 224 0 0 1 0-448m0 64a160.192 160.192 0 0 0-160 160c0 88.192 71.744 160 160 160s160-71.808 160-160-71.744-160-160-160"
+                    ></path>
+                  </svg>
+                </el-icon>
+                <el-icon v-else>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1024 1024"
+                    data-v-ea893728=""
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M876.8 156.8c0-9.6-3.2-16-9.6-22.4-6.4-6.4-12.8-9.6-22.4-9.6-9.6 0-16 3.2-22.4 9.6L736 220.8c-64-32-137.6-51.2-224-60.8-160 16-288 73.6-377.6 176C44.8 438.4 0 496 0 512s48 73.6 134.4 176c22.4 25.6 44.8 48 73.6 67.2l-86.4 89.6c-6.4 6.4-9.6 12.8-9.6 22.4 0 9.6 3.2 16 9.6 22.4 6.4 6.4 12.8 9.6 22.4 9.6 9.6 0 16-3.2 22.4-9.6l704-710.4c3.2-6.4 6.4-12.8 6.4-22.4Zm-646.4 528c-76.8-70.4-128-128-153.6-172.8 28.8-48 80-105.6 153.6-172.8C304 272 400 230.4 512 224c64 3.2 124.8 19.2 176 44.8l-54.4 54.4C598.4 300.8 560 288 512 288c-64 0-115.2 22.4-160 64s-64 96-64 160c0 48 12.8 89.6 35.2 124.8L256 707.2c-9.6-6.4-19.2-16-25.6-22.4Zm140.8-96c-12.8-22.4-19.2-48-19.2-76.8 0-44.8 16-83.2 48-112 32-28.8 67.2-48 112-48 28.8 0 54.4 6.4 73.6 19.2zM889.599 336c-12.8-16-28.8-28.8-41.6-41.6l-48 48c73.6 67.2 124.8 124.8 150.4 169.6-28.8 48-80 105.6-153.6 172.8-73.6 67.2-172.8 108.8-284.8 115.2-51.2-3.2-99.2-12.8-140.8-28.8l-48 48c57.6 22.4 118.4 38.4 188.8 44.8 160-16 288-73.6 377.6-176C979.199 585.6 1024 528 1024 512s-48.001-73.6-134.401-176Z"
+                    ></path>
+                    <path
+                      fill="currentColor"
+                      d="M511.998 672c-12.8 0-25.6-3.2-38.4-6.4l-51.2 51.2c28.8 12.8 57.6 19.2 89.6 19.2 64 0 115.2-22.4 160-64 41.6-41.6 64-96 64-160 0-32-6.4-64-19.2-89.6l-51.2 51.2c3.2 12.8 6.4 25.6 6.4 38.4 0 44.8-16 83.2-48 112-32 28.8-67.2 48-112 48Z"
+                    ></path>
+                  </svg>
+                </el-icon>
+              </span>
             </div>
           </template>
         </el-table-column>
@@ -251,7 +283,7 @@ onMounted(() => {
             <span v-else>{{ scope.row.note }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="操作" width="130px" align="center">
+        <el-table-column label="操作" width="135px" align="center">
           <template #default="scope">
             <el-space v-if="scope.row.__add__">
               <el-button
@@ -271,12 +303,17 @@ onMounted(() => {
                   @click="copyPassword(scope.row.password)"
                   >复制</el-button
                 >
-                <el-button
-                  size="small"
-                  type="danger"
-                  @click="handleDelete(scope.row.domain)"
-                  >删除</el-button
+                <el-popconfirm
+                  hide-icon
+                  title="确认删除吗？"
+                  confirm-button-text="确认"
+                  cancel-button-text="取消"
+                  @confirm="handleDelete(scope.row.domain)"
                 >
+                  <template #reference>
+                    <el-button size="small" type="danger">删除</el-button>
+                  </template>
+                </el-popconfirm>
               </el-space>
             </template>
           </template>
@@ -299,10 +336,6 @@ onMounted(() => {
   background: white;
 }
 
-.visible-btn {
-  color: #81cbf5;
-  background-color: #f8f8f8;
-}
 main {
   position: fixed;
   height: 100%;
@@ -326,7 +359,29 @@ main {
   user-select: none;
   margin-top: 8px;
 }
-.el-custom-form ::v-deep .el-form-item {
+.el-custom-password-column {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-right: 34px;
+}
+.el-custom-password-column .ellipsis {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+.el-custom-password-column:hover .el-custom-hover-icon {
+  display: block;
+}
+.el-custom-hover-icon {
+  display: none;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 20px;
+  cursor: pointer;
+}
+.el-custom-form :deep(.el-form-item) {
   margin-bottom: 0;
 }
 </style>
